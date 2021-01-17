015ac6f21ba4d7687093daa066e90a2b396ed594
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-01-17 13:24:41 +0600

    Revert "Set current culture to en-US"
    This reverts commit f9cb828fd04f7371f87deb3f7c21ccb1fa5d5bbb.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,11 +14,6 @@ namespace StopwatchDesktopApp
         [STAThread]
         static void Main()
         {
-            // Sets the culture to French (France)  
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
-            // Sets the UI culture to French (France)  
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
