d473d5df099467b34a02fdaeca90ba31bc4a9bf2
ttcpip <38619302+ttcpip@users.noreply.github.com>
2021-01-17 13:36:21 +0600

    Added unhandled errors handler

--- Program.cs
+++ Program.cs
@@ -21,7 +21,22 @@ namespace StopwatchDesktopApp
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+
             Application.Run(new FrmMain());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Unhandled Thread Exception");
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception).Message, "Unhandled UI Exception");
         }
     }
 }
